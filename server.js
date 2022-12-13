92b584619917f48983208d752a7a792b3f4a39a7
subhu499 <subhu499@gmail.com>
2022-12-13 17:43:07 +0530

    myntra api

---
+++ server.js
@@ -0,0 +1,18 @@
+// npm init -y
+// npm i json-server
+// create server.js
+// copy template to server.js
+// node server.js
+
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use("/api", router);
+server.listen(PORT, () =>
+  console.log(`JSON Server is running on port ${PORT}`)
+);
